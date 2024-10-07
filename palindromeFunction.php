bbe30b6a3c7478f780c248f967dc106d436df566
FrancescoMinichiello <fraps2278@gmail.com>
2024-10-07 12:32:14 +0200

    added input

---
+++ palindromeFunction.php
@@ -0,0 +1,6 @@
+<?php
+function isPalindrome()
+{
+
+    return isPalindrome();
+}
---
+++ snack-5/palindrome.php
@@ -0,0 +1,18 @@
+<?php include __DIR__("/palindrome.php") ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="palindrome.php" method="GET">
+        <input type="text" name="palindromeWord">
+    </form>
+</body>
+
+</html>
